d2f9cc81a559a3128dd6abd15a77d2cdfb00164d
rmcarvajal <darwyn089@gmail.com>
2025-04-10 07:30:56 -0500

    added API support

--- src/components/card.js
+++ src/components/card.js
@@ -15,12 +15,22 @@ class card extends HTMLElement{
     .image{
     background-image: url(https://cdn-images.dzcdn.net/images/cover/5428172df3ee52b457ccfbfdded21fdb/1900x1900-000000-80-0-0.jpg);
     height: 200px;
-    width: 200px;
+    width: 250px;
     background-size: cover;
     background-color: aqua;
 }
+    .card{
+    display:flex;
+    flex-direction: column;
+    width: 500px;
+}
 </style>
         `;
+    }
+    set data(card) {
+        this.shadowRoot.querySelector(".image").style.backgroundImage = `url(${card.image})`
+        this.shadowRoot.querySelector("h1").textContent = card.name
+        this.shadowRoot.querySelector("h2").textContent = card.description
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import card from "./components/card.js"
+
+
+
+
+
 const addContent = document.getElementById("container")
 
 customElements.define('card-comp', card)
+
+fetch("https://bymykel.com/CSGO-API/api/en/skins.json")
+    .then((response)=> response.json())
+    .then((data) =>{
+
+        const cardContainer = document.querySelector(".container")
+        
+        const content = cardContainer.querySelectorAll("card-comp")
+
+        content.forEach((card,index) => {
+            card.data = data[index + content.length]
+        })
+    }
+)
